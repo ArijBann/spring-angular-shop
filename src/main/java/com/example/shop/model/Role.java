d3ed98b2413ab0ff17136518d6a528cac271c83f
ArijBann <arijbannour45@gmail.com>
2024-01-19 22:26:35 +0100

    Create role and user Entites

---
+++ src/main/java/com/example/shop/model/Role.java
@@ -0,0 +1,14 @@
+package com.example.shop.model;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+@Getter
+@Setter
+@Entity
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+}
